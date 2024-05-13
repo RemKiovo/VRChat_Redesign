d79da20b890326e78d6dd8b6c6a6b870d640266b
LabeVR <stefano.salerno11@gmail.com>
2024-05-13 19:57:34 +0200

    Further background fixed (oops i forgor)

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
     e.preventDefault();
 });
+
+var images = ['background1.jpg', 'background2.jpg'];
+var world = ['Exoplanet Journey', 'VRChat Home World'];
+var authors = ['*Niko', 'VRChat'];
+var index = 0;
+
+setInterval(function() {
+    document.getElementById("main").style.backgroundImage = 'url(\"assets/backgrounds/' + images[index] + '\")';
+    document.getElementById("worldinfo").innerHTML = world[index] + ' by ' + authors[index];
+    index = (index + 1) % images.length;
+}, 10000);
